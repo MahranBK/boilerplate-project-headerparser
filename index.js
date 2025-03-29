c646c29c692239cedaec22afaec60271d1110145
Mahran Ben Khamsa <mahran.bk15@gmail.com>
2025-03-29 12:50:03 +0100

    built a req header parser

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const ipad = req.ip || req.headers['x-forwarded-for'];
+  const lang = req.headers['accept-language'];
+  const softw = req.headers['user-agent'];
+
+  res.json({ipaddress: ipad,language: lang, software: softw});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
